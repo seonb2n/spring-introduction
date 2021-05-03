4bd8df197cca4dc9038305370eb1532b370ace0e
seonb2n <626ksb@gmail.com>
2021-05-03 20:53:25 +0900

    applicationContext 를 활용한 Bean 확인과, Ioc 로 주입된 Bean 의 비교. 둘다 같은 Bean 이다.

--- src/main/java/org/springframework/samples/petclinic/owner/OwnerController.java
+++ src/main/java/org/springframework/samples/petclinic/owner/OwnerController.java
@@ -15,15 +15,13 @@
  */
 package org.springframework.samples.petclinic.owner;
 
+import org.springframework.context.ApplicationContext;
 import org.springframework.samples.petclinic.visit.VisitRepository;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.WebDataBinder;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.InitBinder;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.ModelAndView;
 
 import javax.validation.Valid;
@@ -43,11 +41,21 @@ class OwnerController {
 
 	private final OwnerRepository owners;
 
+	private final ApplicationContext applicationContext;
+
 	private VisitRepository visits;
 
-	public OwnerController(OwnerRepository clinicService, VisitRepository visits) {
+	public OwnerController(OwnerRepository clinicService, VisitRepository visits, ApplicationContext applicationContext) {
 		this.owners = clinicService;
 		this.visits = visits;
+		this.applicationContext = applicationContext;
+	}
+
+	@GetMapping("/bean")
+	@ResponseBody
+	public String bean() {
+		return "bean" +  applicationContext.getBean(OwnerRepository.class)+"\n"
+			+ "owners : " + this.owners;
 	}
 
 	@InitBinder
