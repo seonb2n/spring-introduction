349417152d67cf263a2d184ebd016a59c235d53f
seonb2n <626ksb@gmail.com>
2021-05-04 20:14:40 +0900

    성능 측정을 위한 cashpeft class 에 proxy pattern 을 적용함으로서, client 는 자기의 코드를 바꾸지 않았어도 적용이 가능하다.

--- src/main/java/org/springframework/samples/petclinic/proxy/Cash.java
+++ src/main/java/org/springframework/samples/petclinic/proxy/Cash.java
@@ -1,8 +1,10 @@
 package org.springframework.samples.petclinic.proxy;
 
-public class Cash implements Payment{
+public class Cash implements Payment {
+
 	@Override
 	public void pay(int amount) {
-		System.out.println(amount+" 현금 결제");
+		System.out.println(amount + " 현금 결제");
 	}
+
 }
